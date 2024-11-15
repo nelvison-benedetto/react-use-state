ab3ccfe155d2289dfb44bf7c0301525fc3e8576e
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-11-15 17:54:49 +0100

    working on btns lang

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import AppMain from './components/AppMain'
 import AppFooter from './components/AppFooter'
 
 function App() {
+  const [active, setActive] = useState(0);
+  function handleClick(e){
+    console.log(e.target);
+    const newActive = Number(e.target.getAttribute('data-index'));
+    console.log(newActive);
+    setActive(newActive);
+  }
 
   return (
     <>
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,8 +2,16 @@ import {languages} from '../data/languages'
 
 export default function AppMain(){
     return(
-        <main>
-
+        <main id="">
+            <div className='col-8 card'>
+                <div className='list_btns_lingprogram'>
+                    {languages.map((item,index)=>{})}
+                </div>
+                <div className='description'>
+                    <h2></h2>
+                    <p></p>
+                </div>
+            </div>
         </main>
     );
 }
