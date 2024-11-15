2840f8328ffeb1b9b26d5f645ebb716d8837f259
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-11-15 18:02:02 +0100

    working on ? :

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,11 +1,34 @@
 import {languages} from '../data/languages'
 
 export default function AppMain(){
+
+    const [active, setActive] = useState(0);
+    function handleClick(e){
+      console.log(e.target);
+      const newActive = Number(e.target.getAttribute('data-index'));
+      console.log(newActive);
+      setActive(newActive);
+    }
+  
+    // {accordions.map((item,index)=>(
+    //     <div className='accordion-item' key={item.id}>
+    //       <h2 onClick={handleClick}data-index={index} >{item.title}</h2>
+    //       <div className={active == index? 'content':'hide'}>
+    //         {item.content}
+    //       </div>
+    //     </div>
+    //   ))}
+
     return(
         <main id="">
             <div className='col-8 card'>
                 <div className='list_btns_lingprogram'>
-                    {languages.map((item,index)=>{})}
+                    {languages.map((item,index)=>(
+                        <div className='lang_item' key={item.id}>
+                            <h2 onClick={handleClick} data-index={index}>{item.title}</h2>
+                            <div></div>
+                        </div>
+                    ) )}
                 </div>
                 <div className='description'>
                     <h2></h2>
